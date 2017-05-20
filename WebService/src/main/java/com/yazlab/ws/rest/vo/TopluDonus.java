9ff419571bb4ca2a871d8d3ee9e1590fdd597bde
H.Emre Özhan <emreozhan71@gmail.com>
2017-05-20 15:21:09 +0300

    Directory Change
    Changed directory

---
+++ WebService/src/main/java/com/yazlab/ws/rest/vo/TopluDonus.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.yazlab.ws.rest.vo;
+
+/**
+ *
+ * @author Emre
+ */
+public class TopluDonus {
+    
+    private  JsonTip[] JsonList;
+
+    public JsonTip[] getJsonList() {
+        return JsonList;
+    }
+
+    public void setJsonList(JsonTip[] JsonList) {
+        this.JsonList = JsonList;
+    }
+
+
+
+  
+    
+}
---
+++ WebService/src/main/java/com/yazlab/ws/rest/vo/service/ServisDenemLogin.java
@@ -0,0 +1,88 @@
+package com.yazlab.ws.rest.vo.service;
+
+
+import com.yazlab.ws.rest.vo.DB.SqliteDB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import com.yazlab.ws.rest.vo.JsonTip;
+import com.yazlab.ws.rest.vo.TopluDonus;
+import java.io.File;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+
+
+@Path("/yazlab")
+public class ServisDenemLogin {
+    
+    
+    @GET
+    @Path("/tamliste")
+    ///RestJR/restJR/Yazlab/m/dummy
+    @Produces({javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public TopluDonus ButunEvListele() throws ClassNotFoundException {
+        
+         JsonTip [] ret=new JsonTip[2];   
+         SqliteDB db=new SqliteDB();
+         
+         ret=db.SelectHome();
+         
+         TopluDonus tekli=new TopluDonus();
+         tekli.setJsonList(ret);
+        return tekli;
+    }
+    
+    @GET
+    @Path("/DBCREATE")
+    @Produces("text/plain")
+    @Consumes("text/plain")
+    public String DBKur() throws ClassNotFoundException{
+        
+        SqliteDB db=new SqliteDB();
+        db.DBCreate();
+    return "DB SIFIRLANDI";
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /* çalýþýyor
+    @GET
+    @Path("/m/{msj}")
+   // @Consumes("text/plain")
+    //@Produces("text/plain")
+    @Produces({javax.ws.rs.core.MediaType.APPLICATION_JSON})
+    public String ButunEvListele(@PathParam("msj") String userName) {
+        
+        return "mwsajiniz"+ userName ;
+    }
+	*/
+ /**
+        @POST
+        @Path("/emre")
+	@Consumes({javax.ws.rs.core.MediaType.APPLICATION_JSON})
+	@Produces({javax.ws.rs.core.MediaType.APPLICATION_JSON})
+	public Deneme emre(Deneme vo){
+	
+		vo.setDogru(false);
+               if(vo.getAd().equals("a") && vo.getSoyad().equals("e")){
+                    vo.setDogru(true);
+                }
+                vo.setAd("emreozhan set ad");
+		return vo;
+                
+	}
+        
+ */       
+        
+}
